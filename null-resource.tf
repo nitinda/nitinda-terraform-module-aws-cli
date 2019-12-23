11676c91dd1e7a23501924d89b00f8ddc07c9bcd
nitin-das <nitin.das@thomson.co.uk>
2019-12-23 13:29:21 +0000

    Modified code

--- null-resource.tf
+++ null-resource.tf
@@ -1,11 +1,15 @@
-resource "null_resource" "cli_resource" {
-  provisioner "local-exec" {
-    when    = create
-    command = "/bin/bash -c '${var.role == "" ? "" : "${local.assume_role_cmd} && "}${var.cmd}'"
-  }
+resource "null_resource" "aws_cli_resource" {
+    triggers = {
+        triggers_ids = ""
+    }
 
-  provisioner "local-exec" {
-    when    = destroy
-    command = "/bin/bash -c '${var.role == "" ? "" : "${local.assume_role_cmd} && "}${var.destroy_cmd}'"
-  }
+    provisioner "local-exec" {
+        when    = create
+        command = "/bin/bash -c '${var.role == "" ? "" : "${local.assume_role_cmd} && "}${var.cmd}'"
+    }
+    
+    provisioner "local-exec" {
+        when    = destroy
+        command = "/bin/bash -c '${var.role == "" ? "" : "${local.assume_role_cmd} && "}${var.destroy_cmd}'"
+    }
 }
