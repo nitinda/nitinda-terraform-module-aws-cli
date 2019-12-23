ec3f514af5c5bae79d6f92b32c11d10cac6d5c9c
nitin-das <nitin.das@thomson.co.uk>
2019-12-23 13:26:25 +0000

    Modified code

--- local.tf
+++ local.tf
@@ -1,4 +1,4 @@
 locals {
   account_id      = data.aws_caller_identity.id.account_id
-  assume_role_cmd = "source ${path.module}/assume_role.sh ${local.account_id} ${var.role}"
+  assume_role_cmd = "source ${path.module}/assume-role.sh ${local.account_id} ${var.role}"
 }
