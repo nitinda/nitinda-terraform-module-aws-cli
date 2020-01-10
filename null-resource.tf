ef75a549669bbcd502bc51e4d66086d8ff0fd63e
nitin-das <nitin.das@thomson.co.uk>
2020-01-10 16:41:07 +0000

    Modified Code

--- null-resource.tf
+++ null-resource.tf
@@ -4,12 +4,6 @@ resource "null_resource" "aws_cli_resource" {
     }
 
     provisioner "local-exec" {
-        when    = create
         command = "/bin/bash -c '${var.role == "" ? "" : "${local.assume_role_cmd} && "}${var.cmd}'"
-    }
-
-    provisioner "local-exec" {
-        when    != create
-        command = "/bin/bash -c '${var.role == "" ? "" : "${local.assume_role_cmd} && "}${var.destroy_cmd}'"
     }
 }
--- variables.tf
+++ variables.tf
@@ -2,10 +2,6 @@ variable "cmd" {
   description = "The command used to create the resource."
 }
 
-variable "destroy_cmd" {
-  description = "The command used to destroy the resource."
-}
-
 variable "role" {
   description = "The role to assume in order to run the cli command."
   default     = ""
